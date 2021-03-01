0be8f9e7121cabe555fdef098b0f6cd7dd360e2f
Darryl Davidson <darryl@sbiteam.com>
2021-02-28 19:44:47 -0800

    feat: add cli ui

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -105,6 +105,7 @@ test('de-duplicate cli: should create an output file with an array file', (done)
         '_id',
         '--resolution-key',
         'entryDate',
+        '--silent',
     ]);
     const expected =
         '[{"_id":"jkj238238jdsnfsj23","email":"foo@bar.com","firstName":"John","lastName":"Smith","address":"123 Street St","entryDate":"2014-05-07T17:30:20+00:00"},{"_id":"edu45238jdsnfsj23","email":"mae@bar.com","firstName":"Ted","lastName":"Masters","address":"44 North Hampton St","entryDate":"2014-05-07T17:31:20+00:00"}]';
@@ -147,6 +148,7 @@ test('de-duplicate cli: should create an output file with an object file', (done
         'entryDate',
         '--array-key',
         'leads',
+        '--silent',
     ]);
     const expected =
         '{"leads":[{"_id":"jkj238238jdsnfsj23","email":"foo@bar.com","firstName":"John","lastName":"Smith","address":"123 Street St","entryDate":"2014-05-07T17:30:20+00:00"},{"_id":"edu45238jdsnfsj23","email":"mae@bar.com","firstName":"Ted","lastName":"Masters","address":"44 North Hampton St","entryDate":"2014-05-07T17:31:20+00:00"}]}';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 /* eslint-disable unicorn/no-process-exit */
 /* eslint-disable no-console */
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./types/inquirer-file-tree-selection-prompt/index.d.ts" />
 import { Command, OptionValues } from 'commander';
 import fs from 'fs';
+import inquirer from 'inquirer';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
+import { head } from 'lodash';
+import path from 'path';
 
 import _package from '../package.json';
 import { deDuplicateRecords } from './lodash-service';
@@ -16,6 +22,12 @@ interface IOptions extends OptionValues {
     silent?: boolean;
     takeLast?: boolean;
 }
+
+const getNumberWithOrdinal = (integer: number) => {
+    const suffixes = ['th', 'st', 'nd', 'rd'],
+        module100 = integer % 100;
+    return integer + (suffixes[(module100 - 20) % 10] || suffixes[module100] || suffixes[0]);
+};
 
 const validateSilentOptions = (options: IOptions) => {
     if (!options.in) {
@@ -35,7 +47,131 @@ const validateSilentOptions = (options: IOptions) => {
         process.exit(1);
     }
 };
-
+const promptForArrayKey = async (options: IOptions, input: Record<string, unknown>) => {
+    const keys = Object.keys(input);
+    const { arrayKey } = await inquirer.prompt([
+        {
+            choices: keys,
+            message: 'Select the array property to de-duplicate',
+            name: 'arrayKey',
+            type: 'list',
+        },
+    ]);
+    return Object.assign({}, options, { arrayKey });
+};
+const promptForDetectionKeys = async (
+    options: IOptions,
+    input: Record<string, unknown>,
+    answers: string[] = []
+): Promise<IOptions> => {
+    const finishedChoice = 'Done selecting properties';
+    const keys =
+        answers.length === 0 ? Object.keys(input) : [finishedChoice, ...Object.keys(input)];
+    const ordinal = getNumberWithOrdinal(answers.length + 1);
+    const { detectionKey } = await inquirer.prompt([
+        {
+            choices: keys,
+            message: `Select the ${ordinal} property to use to determine a duplicate record (order does matter in resolving conficts)`,
+            name: 'detectionKey',
+            type: 'list',
+        },
+    ]);
+    if (detectionKey === finishedChoice) {
+        return Object.assign({}, options, { detectionKeys: answers });
+    } else {
+        return promptForDetectionKeys(options, input, [...answers, detectionKey]);
+    }
+};
+const promptForResolutionKey = async (
+    options: IOptions,
+    input: Record<string, unknown>
+): Promise<IOptions> => {
+    const keys = Object.keys(input);
+    const { resolutionKey } = await inquirer.prompt([
+        {
+            choices: keys,
+            message: `Select the property to use to resolve conflicts when duplicates are found`,
+            name: 'resolutionKey',
+            type: 'list',
+        },
+    ]);
+    return Object.assign({}, options, { resolutionKey });
+};
+const promptForTakeLast = async (options: IOptions): Promise<IOptions> => {
+    const { takeLast } = await inquirer.prompt([
+        {
+            message:
+                'When resolving conflicts and the resolution keys are equal Should the last record be selected',
+            name: 'takeLast',
+            type: 'confirm',
+        },
+    ]);
+    return Object.assign({}, options, { takeLast });
+};
+const promptForInputFile = async (options: IOptions): Promise<IOptions> => {
+    const { in: inAnswer } = await inquirer.prompt([
+        {
+            message: 'Select a file to eliminate duplicate records',
+            name: 'in',
+            onlyShowValid: true,
+            type: 'file-tree-selection',
+            validate: (inputFile_1: string) => {
+                return /.+\.json/.test(inputFile_1) || fs.lstatSync(inputFile_1).isDirectory();
+            },
+        },
+    ]);
+    if (fs.lstatSync(inAnswer).isDirectory()) {
+        console.log(`"${inAnswer}" is a directory.  Please choose a JSON file.`);
+        return promptForInputFile(options);
+    }
+    return Object.assign({}, options, { in: inAnswer });
+};
+const promptForOutputFile = async (options: IOptions): Promise<IOptions> => {
+    const directoryName = path.dirname(options.in as string);
+    const defaultOutputName = path.join(directoryName, 'results.json');
+    const { out } = await inquirer.prompt([
+        {
+            default: () => defaultOutputName,
+            message: 'Enter the path to save the result of the de-duplication',
+            name: 'out',
+            type: 'input',
+        },
+    ]);
+    if (fs.existsSync(out) && fs.lstatSync(out).isDirectory()) {
+        console.log(`"${out}" is a directory.  Please choose a JSON file.`);
+        return promptForOutputFile(options);
+    }
+    return Object.assign({}, options, { out });
+};
+const promptUserForMissingOptions = async (options: IOptions): Promise<IOptions> => {
+    let promptOptions = options;
+    if (!promptOptions.in) {
+        promptOptions = await promptForInputFile(promptOptions);
+    }
+    const inputJsonFile = fs.readFileSync(promptOptions.in as string, options.encoding);
+    const input = JSON.parse(inputJsonFile);
+    if (!Array.isArray(input) && !promptOptions.arrayKey) {
+        promptOptions = await promptForArrayKey(promptOptions, input);
+    }
+    const inputArray = promptOptions.arrayKey ? input[promptOptions.arrayKey] : input;
+    const firstRecord = head<Record<string, unknown>>(inputArray);
+    if (firstRecord) {
+        if (!promptOptions.detectionKeys) {
+            promptOptions = await promptForDetectionKeys(promptOptions, firstRecord);
+        }
+        if (!promptOptions.resolutionKey) {
+            promptOptions = await promptForResolutionKey(promptOptions, firstRecord);
+        }
+    }
+    if (promptOptions.takeLast === undefined) {
+        promptOptions = await promptForTakeLast(promptOptions);
+    }
+    if (!promptOptions.out) {
+        promptOptions = await promptForOutputFile(promptOptions);
+    }
+    return promptOptions;
+};
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
 const program = new Command();
 
 program
@@ -61,34 +197,44 @@ program
     .option('-s, --silent', 'disable user interface for automated systems');
 
 program.parse();
-const options = program.opts() as IOptions;
-if (options.silent) {
-    validateSilentOptions(options);
-}
-if (options.in) {
-    if (!fs.existsSync(options.in) && options.silent) {
-        console.error(`file "${options.in}" does not exist`);
-        process.exit(1);
-    }
-    const inputJsonFile = fs.readFileSync(options.in, options.encoding);
-    const input = JSON.parse(inputJsonFile);
-    const records = options.arrayKey ? input[options.arrayKey] : input;
-    if (records === undefined && options.silent) {
-        console.error(`array key "${options.arrayKey}" does not exist in file "${options.in}"`);
-        process.exit(1);
-    }
-    if (options.detectionKeys && options.resolutionKey) {
-        const outputRecords = deDuplicateRecords(
-            records,
-            options.detectionKeys,
-            options.resolutionKey,
-            options.takeLast ?? false
-        );
-        const output = options.arrayKey
-            ? Object.assign({}, input, { [options.arrayKey]: outputRecords })
-            : outputRecords;
-        if (options.out) {
-            fs.writeFileSync(options.out, JSON.stringify(output));
+const commandLineOptions = program.opts() as IOptions;
+Promise.resolve()
+    .then(() => {
+        if (commandLineOptions.silent) {
+            validateSilentOptions(commandLineOptions);
+            return commandLineOptions;
+        } else {
+            return promptUserForMissingOptions(commandLineOptions);
         }
-    }
-}
+    })
+    .then((options: IOptions) => {
+        if (options.in) {
+            if (!fs.existsSync(options.in) && options.silent) {
+                console.error(`file "${options.in}" does not exist`);
+                process.exit(1);
+            }
+            const inputJsonFile = fs.readFileSync(options.in, options.encoding);
+            const input = JSON.parse(inputJsonFile);
+            const records = options.arrayKey ? input[options.arrayKey] : input;
+            if (records === undefined && options.silent) {
+                console.error(
+                    `array key "${options.arrayKey}" does not exist in file "${options.in}"`
+                );
+                process.exit(1);
+            }
+            if (options.detectionKeys && options.resolutionKey) {
+                const outputRecords = deDuplicateRecords(
+                    records,
+                    options.detectionKeys,
+                    options.resolutionKey,
+                    options.takeLast ?? false
+                );
+                const output = options.arrayKey
+                    ? Object.assign({}, input, { [options.arrayKey]: outputRecords })
+                    : outputRecords;
+                if (options.out) {
+                    fs.writeFileSync(options.out, JSON.stringify(output));
+                }
+            }
+        }
+    });
